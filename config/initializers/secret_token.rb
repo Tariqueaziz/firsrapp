625b01249dcf73b078c40670fdda232054bcb423
root <plutomercury.com>
2013-07-10 16:49:26 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firsrapp::Application.config.secret_token = '34051b332a37935ac2876d5014e069d3cb8bb77ffea2d1d811f8cb39cf1ba182b3249fe638d4cbec08b246512ed152294a90dbf2633a27adf2459dee1dc01949'
